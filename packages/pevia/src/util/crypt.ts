3823f7de2281612e4365c2cc1eeeeaf565044987
byimaan <byimaan1@gmail.com>
2025-08-27 20:20:20 -0600

    Introducing pipelines <DS> in project to better handle the flow of complex actions in sequence

---
+++ packages/pevia/src/util/crypt.ts
@@ -0,0 +1,28 @@
+import {createHash} from "node:crypto";
+import { windowSafe } from "./fs";
+
+//produce safe slugs for files
+export const slugify = (
+    s:string,opts={def:'',winSafe:true}
+):string => {
+    const base = s
+        .normalize("NFKD") // must be a consistent unicode form
+        .replace(/[^\p{Letter}\p{Number}]+/gu, "-") //non-letters->'-'
+        .replace(/^-+|-+$/g, "") // trim('-') if exist
+        .toLowerCase() || opts.def || "not-defined";
+    return opts.winSafe ? windowSafe(base) : base
+};
+
+
+//compute sha256 & short hash for filenames
+export const sha256 = (
+    buf:Uint8Array
+):string => {
+    return createHash('sha256').update(buf).digest('hex')
+};
+export const hash8 = (
+    hex:string
+):string => {
+    return hex.slice(0,8)
+};
+
---
+++ packages/pevia/src/util/pipeline.ts
@@ -0,0 +1,97 @@
+
+export type PipelinePayload<G,S> = {
+    pipeName: string,
+    pipeDescription:string,
+    globals : G,
+    stages: S
+};
+
+export type StageResult<T, M> = 
+  | { ok: true;  returnValue: T; metadata?: {ms?:number} & M }
+  | { ok: false; error: Error;  metadata?: {ms?:number} & M };
+
+
+export type StageCallback<P,T,M> = (getPayload:()=>P) => Promise<
+    StageResult<T,M>
+>;
+
+export type PipelineStage<T/*ReturnType*/,M/*METADATA*/> = {
+    description:string;
+    res: StageResult<T,M>
+};
+
+class Pipeline <
+    G,/**GLOBALS*/
+    S/**STAGES*/
+> {
+    constructor(private payload:PipelinePayload<G,S>){
+        this.payload=payload;
+    };
+
+    async middleware(
+        //Main purpose of middleware is to update payload before it get passed to upcoming stages
+        // & can do any type of verification check 
+        cfn: (getPayload:() => typeof this.payload) => Promise<typeof this.payload>
+    ){
+        this.payload = await cfn(
+            () => {return this.payload}
+        )
+        return this;
+    };
+
+    async stage<T,M>(
+        name:string,
+        description:string,
+        cfn:StageCallback<typeof this.payload,T,M>,
+        handleError=(error:Error)=>{throw error}
+    ){
+        const t0 = Date.now();
+        try { 
+            const res = await cfn(
+                () => {return this.payload}
+            );
+            
+            if (res.metadata === undefined) res.metadata = {} as {}&M;
+            res.metadata.ms = Date.now()-t0;
+
+            this.payload.stages[name] = {
+                description,
+                res
+            }
+            
+        } catch (error) {
+            this.payload.stages[name] = {
+                description,
+                res:{
+                    ok:false,
+                    error,
+                    metadata: {
+                        ms: Date.now()-t0
+                    }
+                }
+            }
+            handleError(error); //Easily pluggable error handler 
+        };
+
+        return this
+    };
+
+    async launch(){
+        return this.payload
+    }
+};
+
+export const pipeline = <G,S>(pipeName:string) => {
+    return (pipeDescription:string) => {
+        return (
+            args:{globals:G;stages:S}
+        ) => (
+            new Pipeline<G,S>({
+                pipeName,
+                pipeDescription,
+                globals:args.globals,
+                stages:args.stages,
+            })
+        )
+    }
+};
