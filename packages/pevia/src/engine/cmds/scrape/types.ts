5511fb140339e8510eb03f4ee935ccb37bf312d5
byimaan <byimaan1@gmail.com>
2025-08-31 18:53:22 -0600

    Types/interfaces needed to build SCRAPE_PIPELINE

---
+++ packages/pevia/src/engine/cmds/scrape/types.ts
@@ -0,0 +1,79 @@
+import { CheerioAPI } from "cheerio";
+import { Response as UResponse } from "undici";
+import { Candidate,Page, } from "../../types";
+import { ResolveConfig } from "../../../config/schema";
+import { PipelineStage,PipelinePayload } from "../../../util/pipeline";
+
+
+/*****************************SCRAPE-PIPELINE*****************************/
+
+export type ScrapeGlobals = {
+    page?:Page;
+    url:string;
+    cfg:ResolveConfig;
+};
+
+export type ScrapeStages = {
+    'renderer': 
+        PipelineStage<
+            {$:CheerioAPI,html:string,text:string},/*RETURN-VALUE*/
+            {res?:UResponse}/*OPTIONAL-METADATA*/
+        >;
+    'extractor':
+        PipelineStage<
+            {candidates:Candidate[]},/*RETURN-VALUE*/
+            {}/*METADATA*/
+        >;
+    'filterer':
+        PipelineStage<
+            {filteredCandidates:Candidate[]},/*RETURN-VALUE*/
+            {}/*METADATA*/
+        >;
+    'fs-manager':
+        PipelineStage<
+            {outDirAbs:string;manifestOutAbs:string,history:FileWriteHistory}, /*RETURN-VALUE*/
+            {stats?:{downloaded:number;failed:number;skipped:number}}/*OPTIONAL-METADATA*/
+        >;
+};
+
+export type ScrapePayload = PipelinePayload<ScrapeGlobals,ScrapeStages>
+
+
+
+/*****************************FILESYSTEM*****************************/
+export type DownloadOK = { 
+    ok:true;
+    buffer:Uint8Array;
+    mime?:string;
+    finalUrl?:string;
+    metadata: {ms:number}
+};
+
+export type DownloadNotOK = {
+    ok:false;
+    error:Error;
+    metadata:{ms:number}
+};
+//downloadBinary
+export type DownloadReturnType = DownloadOK | DownloadNotOK;
+
+type ManifestRow = Candidate & {
+    topic:string;
+    ts:string;
+    ms?:number;
+};
+
+export type SavedRow = ManifestRow & {
+    status:'saved',
+    hash8:string;
+    ext:string;
+    mime:string;
+    bytes:number;
+    savedPath:string
+}
+
+export type FileWriteHistory = {
+    'saved': SavedRow[];
+    'failed': (ManifestRow&{status:'failed',reason:string,})[];
+    'skipped': (ManifestRow&{status:'skipped',topic?:string,reason:string})[]
+}
